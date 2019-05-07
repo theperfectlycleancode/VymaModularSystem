8e296f7f53b798a0adbdac4951d55063db9f9a46
theperfectlycleancode <samgamesx.sam@gmail.com>
2019-05-07 23:58:23 +0200

    ExceptionHandler update

--- src/main/java/me/samuel/vyma/backend/core/vmodules/modules/exceptionHandler/VExceptionHandler.java
+++ src/main/java/me/samuel/vyma/backend/core/vmodules/modules/exceptionHandler/VExceptionHandler.java
@@ -1,25 +1,55 @@
 package me.samuel.vyma.backend.core.vmodules.modules.exceptionHandler;
 
 import me.samuel.vyma.backend.core.vmodules.VModule;
+import me.samuel.vyma.backend.core.vmodules.modules.logger.loggers.VExceptionLogger;
 
 public abstract class VExceptionHandler extends VModule implements Thread.UncaughtExceptionHandler {
 
+    /**
+     * Should the handler log exceptions
+     */
+    private boolean shouldLog;
+
+    private VExceptionLogger exceptionLogger;
 
     /**
      * VModule base constructor
      *
-     * @param name
-     * @param version
+     * @param name VExceptionHandler module name
+     * @param version version
      */
     public VExceptionHandler(String name, double version) {
         super(name, version);
+        this.shouldLog = true;
+
+        this.exceptionLogger = new VExceptionLogger();
+        this.exceptionLogger.setStream(System.out);
     }
 
-    public abstract void onException(Thread thread, Throwable throwable);
+    abstract void onException(Thread thread, Throwable throwable);
 
     @Override
     public void uncaughtException(Thread thread, Throwable throwable) {
+        this.exceptionLogger.onException(thread, throwable);
         onException(thread, throwable);
+    }
+
+    /**
+     * Enables exceptionLogger
+     */
+    public void enableLogger() {
+        this.shouldLog = true;
+    }
+
+    /**
+     * Disables exceptionLogger
+     */
+    public void disableLogger() {
+        this.shouldLog = false;
+    }
+
+    public VExceptionLogger getExceptionLogger() {
+        return this.exceptionLogger;
     }
 
     @Override
--- src/main/java/me/samuel/vyma/backend/core/vmodules/modules/logger/loggers/VExceptionLogger.java
+++ src/main/java/me/samuel/vyma/backend/core/vmodules/modules/logger/loggers/VExceptionLogger.java
@@ -1,6 +1,7 @@
 package me.samuel.vyma.backend.core.vmodules.modules.logger.loggers;
 
 import me.samuel.vyma.backend.core.vmodules.modules.logger.VLogger;
+import me.samuel.vyma.backend.core.vmodules.modules.logger.VLoggerMessage;
 
 public class VExceptionLogger extends VLogger {
 
@@ -11,5 +12,16 @@ public class VExceptionLogger extends VLogger {
     public VExceptionLogger() {
         super("VExceptionLogger", 1.0);
         this.updatePrefix("EXCEPTION");
+    }
+
+    public void onException(Thread thread, Throwable throwable) {
+        String[] message = new String[] {
+                "Thread: "+thread.getName()+" : "+thread.getId(),
+                "Exception: "+throwable.toString(),
+                "Message: "+throwable.getMessage()
+        };
+        VLoggerMessage loggerMessage = new VLoggerMessage(message);
+        this.log(loggerMessage);
+        throwable.printStackTrace(this.getStream());
     }
 }
